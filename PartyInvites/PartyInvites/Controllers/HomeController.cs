410b1c8dcb0782d9d8848fb56e70b327cef8c433
mmdu <mingming.du@gmail.com>
2017-04-19 13:07:10 -0500

    RsvpForm Submit

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "good morning" : "good afternoon";
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
